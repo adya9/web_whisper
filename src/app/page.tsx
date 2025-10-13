2be5d2e034d412ccf3a60ac2dbedc7db9c461713
adya9 <adyakumari2001@gmail.com>
2025-10-13 18:42:09 +0530

    Added Vapi setup + basic communication widget

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+import VapiWidget from "./VapiWidget";
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-     ADYA
+      <VapiWidget
+        apiKey={process.env.apikey || ""}
+        assistantId={process.env.assistantId || ""}
+      />
     </div>
   );
 }
